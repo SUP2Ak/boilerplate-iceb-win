ea61e52c20a48f647e46a15cc89bc02c0c3b9d8f
SUP2Ak <cormier.wesley@gmail.com>
2024-11-12 17:14:52 +0100

    ✨ feat(main): implement core structure for MyApp
    - add update and view methods to manage application state
    - create a default implementation for MyApp
    - enhance main function to display application name with version
    - improve icon loading error messages for better clarity

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,15 @@
-#![windows_subsystem = "windows"] 
+#![windows_subsystem = "windows"] // I think I can remove this line in the futur I need more test when I create the installer if application got cmd window or not
 
-//use std::{f32::consts::PI, time::Instant};
+// I will release .iss file for the installer later
+
 use iced::{Element, Theme, window, Size};
 
 struct MyApp;
 
-pub fn main() -> iced::Result {
-    let version: &str = env!("CARGO_PKG_VERSION");
-    let title_string = format!("everysup - v{}", version);
-    let title: &'static str = Box::leak(title_string.into_boxed_str());
-
-    // Charger et décoder l'icône PNG
-    let icon_bytes = include_bytes!("../assets/Logo_SupV.ico");
-    let image = image::load_from_memory(icon_bytes)
-        .expect("Impossible de charger l'image")
-        .into_rgba8();
-    let (width, height) = image.dimensions();
-    
-    let icon = window::icon::from_rgba(image.into_raw(), width, height)
-        .expect("Impossible de créer l'icône");
-
-    let mut settings = window::Settings::default();
-    settings.size = Size::new(1024.0, 768.0);
-    settings.icon = Some(icon);
-    settings.resizable = true;
-    settings.decorations = true;
-    settings.position = window::Position::Centered;
-
-    iced::application(
-        title,
-        MyApp::update,
-        MyApp::view
-    )
-        .theme(|_| Theme::Dark)
-        .antialiasing(true)
-        .window(settings)
-        .run()
-}
-
+// Update and view implementation for the application
 impl MyApp {
     fn update(&mut self, _: ()) {
-        // Pas de retour, juste une mise à jour de l'état si nécessaire
+        // No return, just an update of the state if necessary
     }
 
     fn view(&self) -> Element<()> {
@@ -53,8 +22,46 @@ impl MyApp {
     }
 }
 
+// Default implementation
 impl Default for MyApp {
     fn default() -> Self {
         Self
     }
+}
+
+// Main function for the application
+fn main() -> iced::Result {
+    // If you want to display something from the environment (like name - version)
+    let version: &str = env!("CARGO_PKG_VERSION");
+    let name: &str = env!("CARGO_PKG_NAME");
+    let title_string = format!("{} - v{}", name, version);
+    let title: &'static str = Box::leak(title_string.into_boxed_str());
+
+    // Load the icon (.ico is better)
+    let icon_bytes = include_bytes!("../assets/Logo_SupV.ico");
+    let image = image::load_from_memory(icon_bytes)
+        .expect("Impossible to load the image")
+        .into_rgba8();
+    let (width, height) = image.dimensions();
+    
+    let icon = window::icon::from_rgba(image.into_raw(), width, height)
+        .expect("Impossible to create the icon");
+
+    let mut settings = window::Settings::default();
+    settings.size = Size::new(1024.0, 768.0);
+    settings.icon = Some(icon);
+    settings.resizable = true;
+    settings.decorations = true;
+    settings.position = window::Position::Centered;
+
+    // Create the application
+    iced::application(
+        title,
+        MyApp::update,
+        MyApp::view
+    )
+        .theme(|_| Theme::Dark)
+        .antialiasing(true)
+        .window(settings)
+        .run()
 }
